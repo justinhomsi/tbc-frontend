58df3654e18136de3d5aa0dd83aaea3741869a2a
Justin Homsi <justin_homsi@hotmail.com>
2020-07-24 19:38:33 +0800

    Moved Patch Notes Table to Own Component

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -2,31 +2,14 @@
   <div><h1>Original Patch Notes</h1>
   <br />
   <thead><h2>Patch 2.0: Before the Storm / The Burning Crusade</h2></thead>
-    <v-simple-table dark dense class="notesTable">
-      <tbody>
-        <tr>
-        <td
-          v-for="(item, index) in notes_data.two_zero"
-          :key="index"
-          dark
-          dense
-          :to="item.link"
-          style="text-align: center; font-size: 18px"
-        >
-        <template>
-          <router-link :to="item.link">{{ item.title }}</router-link>
-        </template>
-        <v-col style="font-size: 15px">Released (US): {{ item.release }}</v-col>
-        </td>
-        </tr>
-      </tbody>
-    </v-simple-table>
+  <NotesTable :data="notes_data.two_zero"></NotesTable>
     <br />
     <thead><h2>Patch 2.1: The Black Temple</h2></thead>
   </div>
 </template>
 
 <script>
+import NotesTable from './NotesTable.vue';
 const axios = require('axios')
 var notes_data;
 
@@ -39,6 +22,9 @@ export default {
   },
   mounted() {
     this.notes_data = require('../assets/notes_data.json')
+  },
+  components: {
+    NotesTable
   }
 }
 </script>
---
+++ src/components/NotesTable.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-simple-table dark dense class="notesTable">
+      <tbody>
+        <tr>
+        <td
+          v-for="(item, index) in data"
+          :key="index"
+          dark
+          dense
+          :to="item.link"
+          style="text-align: center; font-size: 18px"
+        >
+      <template>
+        <router-link :to="item.link">{{ item.title }}</router-link>
+      </template>
+        <v-col style="font-size: 15px">Released (US):<br /> {{ item.release }}</v-col>
+    </td>
+        </tr>
+      </tbody>
+    </v-simple-table>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {}
+  }
+}
+</script>
