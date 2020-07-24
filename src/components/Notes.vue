5637b2539e1e518f1a9674e0961ca373411bb015
Justin Homsi <justin_homsi@hotmail.com>
2020-07-24 20:04:25 +0800

    Added all Original Patch Notes

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,10 +1,20 @@
 <template>
   <div><h1>Original Patch Notes</h1>
-  <br />
-  <thead><h2>Patch 2.0: Before the Storm / The Burning Crusade</h2></thead>
-  <NotesTable :data="notes_data.two_zero"></NotesTable>
+    <br />
+    <thead><h2>Patch 2.0: Before the Storm / The Burning Crusade</h2></thead>
+      <NotesTable :data="notes_data.two_zero"></NotesTable>
     <br />
     <thead><h2>Patch 2.1: The Black Temple</h2></thead>
+      <NotesTable :data="notes_data.two_one"></NotesTable>
+    <br />
+    <thead><h2>Patch 2.2: Voice Chat!</h2></thead>
+      <NotesTable :data="notes_data.two_two"></NotesTable>
+    <br />
+    <thead><h2>Patch 2.3: The Gods of Zul'Aman</h2></thead>
+      <NotesTable :data="notes_data.two_three"></NotesTable>
+    <br />
+    <thead><h2>Patch 2.4: Fury of the Sunwelln</h2></thead>
+      <NotesTable :data="notes_data.two_four"></NotesTable>
   </div>
 </template>
 
